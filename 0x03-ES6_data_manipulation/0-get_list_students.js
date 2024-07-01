59fd78a5f829a77edd631815b81c6271d655562b
Amanda Mabunda <amandamabu14@gmail.com>
2024-07-01 18:16:32 +0000

    task 0

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,12 @@
+// creating a function with three attributes
+// that return an array of the objects created
+// which is a list of students
+
+export default function getListStudents() {
+	return [
+	 { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+	 { id: 2, firstName: 'James', location: 'Columbia' },
+	 { id: 5, firstName: 'Serena', location: 'San Francisco' },
+	];
+}
+	
